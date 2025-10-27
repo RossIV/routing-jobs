72e07ec567c8be494f8051cbfce44c5df220b717
Ryan Vredenburg <ryanvredenburg@es.net>
2025-10-27 07:03:06 -0500

    Now with more tenant grouping

--- jobs/management_interfaces.py
+++ jobs/management_interfaces.py
@@ -43,7 +43,7 @@ class CreateManagementInterface(Job):
         display_field="name",
         query_params={
             "status": "Active",
-            "tenant": "SCinet"
+            "tenant_group": "SCinet"
         },
     )
 
@@ -255,7 +255,7 @@ class ConfigureExistingInterfaceForManagement(Job):
         display_field="name",
         query_params={
             "status": "Active",
-            "tenant": "SCinet"
+            "tenant_group": "SCinet"
         },
     )
 
