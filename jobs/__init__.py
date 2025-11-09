36490c691e253b2fd8a990bb7d99aa4c99a13fb9
Ross Lindsay <ross.lindsay@oit.gatech.edu>
2025-11-08 20:54:00 -0500

    Add job definition for bulk

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -4,12 +4,13 @@ from nautobot.core.celery import register_jobs
 
 from .management_interfaces import CreateManagementInterface, ConfigureExistingInterfaceForManagement
 from .exhibitor_connection import CreateExhibitorConnection
-from .loopback_interfaces import CreateLoopbackInterface
+from .loopback_interfaces import CreateLoopbackInterface, BulkCreateLoopbackInterfaces
 
 jobs = [
     CreateManagementInterface,
     ConfigureExistingInterfaceForManagement,
     CreateExhibitorConnection,
     CreateLoopbackInterface,
+    BulkCreateLoopbackInterfaces
 ]
 register_jobs(*jobs)
